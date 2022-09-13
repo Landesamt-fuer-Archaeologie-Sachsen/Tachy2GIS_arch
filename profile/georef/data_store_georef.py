0d601614be9d15d2aaa551c56eaa2f2159174a38
Gittest <mario.uhlig@visdat.de>
2022-09-13 07:12:50 +0200

    fix georef

--- profile/georef/data_store_georef.py
+++ profile/georef/data_store_georef.py
@@ -90,41 +90,85 @@ class DataStoreGeoref():
     def addAarPoints(self, aarList):
         print('jetzt_addAarPoints', aarList)
 
-        aarArray = []
-        min_x_array = []
-        #Punkte
-        for pointObj in aarList['coord_trans']:
+        aarDirection = aarList['aar_direction']
 
-            aarArray.append({
-                'uuid': pointObj[8],
-                'ptnr': str(pointObj[7]),
-                'x': pointObj[0],
-                'y': pointObj[1],
-                'z': pointObj[2],
-                'z_org': pointObj[4],
-                'distance': pointObj[5],
-                'usage': pointObj[6]
-            })
+        if aarDirection == 'original':
 
-            min_x_array.append(pointObj[0])
+            aarArray = []
+            #Punkte
+            for pointObj in aarList['coord_trans']:
 
-        self.pup.publish('pushAarPoints', aarArray)
+                aarArray.append({
+                    'uuid': pointObj[8],
+                    'ptnr': str(pointObj[7]),
+                    'x': pointObj[0],
+                    'y': pointObj[1],
+                    'z': pointObj[2],
+                    'z_org': pointObj[4],
+                    'distance': pointObj[5],
+                    'usage': pointObj[6]
+                })
 
-        self.aarPointsOriginal = aarArray
-        self.aarPointsAbsolute = aarArray
-        self.aarPointsHorizontal = aarArray
+            self.pup.publish('pushAarPoints', aarArray)
 
-        #Transformationsparameter
-        transformationParams = aarList['transformationParams']
-        z_slope = aarList['linegress'][0]
-        z_intercept = aarList['linegress'][1]
-        transformationParams['z_slope'] = z_slope
-        transformationParams['z_intercept'] = z_intercept
-        transformationParams['ns_error'] = aarList['ns_error']
+            self.aarPointsOriginal = aarArray
+            
+        if aarDirection == 'absolute height':
 
-        transformationParams['min_x'] = min(min_x_array)
+            aarArray = []
+            #Punkte
+            for pointObj in aarList['coord_trans']:
 
-        self.updateAarTransformationParams(transformationParams)            
+                aarArray.append({
+                    'uuid': pointObj[8],
+                    'ptnr': str(pointObj[7]),
+                    'x': pointObj[0],
+                    'y': pointObj[1],
+                    'z': pointObj[2],
+                    'z_org': pointObj[4],
+                    'distance': pointObj[5],
+                    'usage': pointObj[6]
+                })
+
+            self.pup.publish('pushAarPoints', aarArray)
+
+            self.aarPointsAbsolute = aarArray
+
+        if aarDirection == 'horizontal':
+
+            aarArray = []
+            min_x_array = []
+            #Punkte
+            for pointObj in aarList['coord_trans']:
+
+                aarArray.append({
+                    'uuid': pointObj[8],
+                    'ptnr': str(pointObj[7]),
+                    'x': pointObj[0],
+                    'y': pointObj[1],
+                    'z': pointObj[2],
+                    'z_org': pointObj[4],
+                    'distance': pointObj[5],
+                    'usage': pointObj[6]
+                })
+
+                min_x_array.append(pointObj[0])
+
+            self.pup.publish('pushAarPoints', aarArray)
+
+            self.aarPointsHorizontal = aarArray   
+
+            #Transformationsparameter
+            transformationParams = aarList['transformationParams']
+            z_slope = aarList['linegress'][0]
+            z_intercept = aarList['linegress'][1]
+            transformationParams['z_slope'] = z_slope
+            transformationParams['z_intercept'] = z_intercept
+            transformationParams['ns_error'] = aarList['ns_error']
+
+            transformationParams['min_x'] = min(min_x_array)
+
+            self.updateAarTransformationParams(transformationParams)        
 
 
     def updateAarTransformationParams(self, params):
