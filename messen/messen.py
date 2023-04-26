0820cf000a63ec6cbd75ea32b5920c44ec853dbd
mauhvi <mario.uhlig@visdat.de>
2023-04-26 15:54:38 +0200

    Tachy2GIS plugin naming

--- messen/messen.py
+++ messen/messen.py
@@ -92,13 +92,15 @@ class Measurement():
         self.__dockwidget.butClear.clicked.connect(self.__koordtableClear)
         self.__dockwidget.tabWidget_2.currentChanged.connect(self.__watchEventStop)
 
-        # print(active_plugins)
+        #print(active_plugins)
         # active_plugins enthält einmal:
         # ['firstaid', 'plugin_reloader', 'Tachy2GIS', 'Tachy2GIS_arch', 'grassprovider', 'processing']
         # und einmal: (nach einem plugin reload)
         # ['firstaid', 'plugin_reloader', 'Tachy2GIS_arch', 'grassprovider', 'processing', 'Tachy2GIS']
-        # >"Tachy2GIS" in s< passt auf 'Tachy2GIS_arch' und auf 'Tachy2GIS'
-        self.__t2gInstance = plugins.get([s for s in active_plugins if "Tachy2GIS" == s][0])
+        # und einmal: (nach einem plugin reload)
+        # ['firstaid', 'plugin_reloader', 'Tachy2GIS_arch', 'grassprovider', 'processing', 'Tachy2GIS-3D_viewer']
+        # Es soll entweder das Tachy2GIS oder das Tachy2GIS-3D_viewer Plugin verwendet werden aber nicht das Tachy2GIS_arch Plugin
+        self.__t2gInstance = plugins.get([s for s in active_plugins if "Tachy2GIS" in s and "Tachy2GIS_arch" != s][0])
         self.__t2gInstance.dlg.closingPlugin.connect(self.__t2gInstanceClose)
 
         self.__vertices = []#self.__t2gInstance.vtk_mouse_interactor_style.vertices
