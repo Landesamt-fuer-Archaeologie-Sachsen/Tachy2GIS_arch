a055be3e1571fd3313b503032c272841c42f79db
michael-visdat <-@>
2023-06-30 16:11:39 +0200

    weißer Hintergrund statt schwarzer; JPEG-Qualität auf 100 %

--- profile/georef/image_georef.py
+++ profile/georef/image_georef.py
@@ -1,71 +1,33 @@
-# -*- coding: utf-8 -*-
-import os
-from osgeo import gdal, osr
 from PIL import Image
 import numpy as np
 import math
 
-from qgis.core import (
-    QgsVectorLayer,
-    QgsField,
-    QgsVectorFileWriter,
-    QgsCoordinateReferenceSystem,
-    QgsFeature,
-    QgsPointXY,
-    QgsGeometry,
-)
+from qgis.core import QgsPointXY
 from qgis.analysis import QgsGcpTransformerInterface
-from PyQt5.QtCore import QVariant
-
-from ..rotation_coords import RotationCoords
 
 
 class ImageGeoref:
-    def __init__(self, dialogInstance, dataStoreGeoref, iFace):
-        self.iconpath = os.path.join(os.path.dirname(__file__), "..", "Icons")
-
-        self.dialogInstance = dialogInstance
-
-        self.imageFileIn = ""
-
-        self.imageFileOut = ""
-
-        self.gcpPoints = ""
-
-        self.dataStoreGeoref = dataStoreGeoref
-
-        self.__iface = iFace
-
+    def __init__(self):
+        self.image_file_in = ""
+        self.image_file_out = ""
+        self.gcp_points = ""
         self.crs = ""
 
-    def runGeoref(self, georefData, crs, imageFileIn, imageFileOut):
-        self.imageFileIn = imageFileIn
-        self.imageFileOut = imageFileOut
-
+    def run_georef(self, georefData, crs, imageFileIn, imageFileOut):
+        self.image_file_in = imageFileIn
+        self.image_file_out = imageFileOut
+        self.gcp_points = georefData
         self.crs = crs
-        retVal = "ok"
-        self.gcpPoints = georefData
 
-        if len(self.gcpPoints) > 3:
-            self.__startProjective()
-            # self.createGcpShape()
-        else:
-            retVal = "error"
-            self.__iface.messageBar().pushMessage(
-                "Hinweis",
-                "Konnte Profil nicht georeferenzieren. Es müssen min. 4 GCP gesetzt sein!",
-                level=1,
-                duration=5,
-            )
+        if len(self.gcp_points) < 4:
+            return "error"
 
-        return retVal
+        self._start_projective()
+        return "ok"
 
-    ## \brief Start georeferencing dialog
-    #
-    #
-    def __startProjective(self):
+    def _start_projective(self):
         # source image
-        with Image.open(self.imageFileIn) as imgOrg:
+        with Image.open(self.image_file_in) as imgOrg:
             imgOrg.load()
         src_w, src_h = imgOrg.size
         imgOrg.close()
@@ -80,19 +42,13 @@ class ImageGeoref:
 
         src_cp = []
         geo_cp = []
-        for point in self.gcpPoints:
-            src_cp.append(
-                QgsPointXY(float(point["input_x"]), float(point["input_z"]) * -1)
-            )
+        for point in self.gcp_points:
+            src_cp.append(QgsPointXY(float(point["input_x"]), float(point["input_z"]) * -1))
             geo_cp.append(QgsPointXY(float(point["aar_x"]), float(point["aar_z"])))
 
         # (1) get matrix for transformation T1 (src → geo) from 4...n control points
-        transformMethod = QgsGcpTransformerInterface.TransformMethod(
-            6
-        )  # 6 - projective
-        qgisTransformer = QgsGcpTransformerInterface.createFromParameters(
-            transformMethod, src_cp, geo_cp
-        )
+        transformMethod = QgsGcpTransformerInterface.TransformMethod(6)  # 6 - projective
+        qgisTransformer = QgsGcpTransformerInterface.createFromParameters(transformMethod, src_cp, geo_cp)
 
         # get extent of destination image in georeferenced area
         # transformation of source image vertices with transformation T1
@@ -167,15 +123,15 @@ class ImageGeoref:
             [tuple(dst_vp_ul), tuple(dst_vp_ur), tuple(dst_vp_lr), tuple(dst_vp_ll)],
             (1, -1),
         )
-        coeffs = self.__find_pill_coeffs(projectiveCoords, imgCoords)
+        coeffs = self._find_pill_coeffs(projectiveCoords, imgCoords)
 
-        self.__imgTransform(self.imageFileIn, self.imageFileOut, coeffs, dst_h, dst_w)
-        worldfilePath = self.imageFileOut[:-3] + "wld"
+        self._img_transform(self.image_file_in, self.image_file_out, coeffs, dst_h, dst_w)
+        worldfilePath = self.image_file_out[:-3] + "wld"
 
-        self.__writeWorldFile(worldfilePath, geo_ul, res_img)
+        self._write_world_file(worldfilePath, geo_ul, res_img)
 
-    # where pb is the four vertices in the current plane, and pa contains four vertices in the resulting plane.
-    def __find_pill_coeffs(self, pa, pb):
+    def _find_pill_coeffs(self, pa, pb):
+        # where pb is the four vertices in the current plane, and pa contains four vertices in the resulting plane.
         matrix = []
         for p1, p2 in zip(pa, pb):
             matrix.append([p1[0], p1[1], 1, 0, 0, 0, -p2[0] * p1[0], -p2[0] * p1[1]])
@@ -187,17 +143,21 @@ class ImageGeoref:
         res = np.dot(np.linalg.inv(A.T * A) * A.T, B)
         return np.array(res).reshape(8)
 
-    def __imgTransform(self, image_in, image_out, coeffs, heightDst, widthDst):
+    def _img_transform(self, image_in, image_out, coeffs, heightDst, widthDst):
         with Image.open(image_in) as img:
             img.load()
 
         img.transform(
-            (int(widthDst), int(heightDst)), Image.PERSPECTIVE, coeffs, Image.BICUBIC
-        ).save(image_out)
+            size=(int(widthDst), int(heightDst)),
+            method=Image.PERSPECTIVE,
+            data=coeffs,
+            resample=Image.BICUBIC,
+            fillcolor="white",
+        ).save(image_out, quality=100)
 
         img.close()
 
-    def __writeWorldFile(self, worldfilePath, geo_ul, res_img):
+    def _write_world_file(self, worldfilePath, geo_ul, res_img):
         pixelwidth = res_img
         pixelheight = res_img * -1
 
@@ -213,112 +173,3 @@ class ImageGeoref:
             for line in lines:
                 f.write(line)
                 f.write("\n")
-
-    def startTranslate(self):
-        tempOut = self.imageFileOut[:-4]
-        tempOut = tempOut + "_translate.tif"
-        # Create a copy of the original file and save it as the output filename:
-
-        gdal.Translate(tempOut, self.imageFileIn)
-
-        # Open the output file for writing:
-        ds = gdal.Open(tempOut, gdal.GA_Update)
-        # Set spatial reference:
-        sr = osr.SpatialReference()
-        sr.ImportFromEPSG(self.crs.postgisSrid())  # z.B 31468
-
-        # Enter the GCPs
-        #   Format: [map x-coordinate(longitude)], [map y-coordinate (latitude)], [elevation],
-        #   [image column index(x)], [image row index (y)]
-
-        gcps_aar = []
-        for point in self.gcpPoints:
-            gcps_aar.append(
-                gdal.GCP(
-                    float(point["aar_x"]),
-                    float(point["aar_z"]),
-                    0,
-                    float(point["input_x"]),
-                    float(point["input_z"]),
-                )
-            )
-
-        # Apply the GCPs to the open output file:
-
-        ds.SetGCPs(gcps_aar, sr.ExportToWkt())
-
-        gdal.Warp(
-            self.imageFileOut,
-            ds,
-            srcSRS=self.crs.authid(),
-            dstSRS=self.crs.authid(),
-            options="-overwrite -r bilinear -order 1 -co compress=none -co worldfile=yes",
-        )
-
-        # Close the output file in order to be able to work with it in other programs:
-        ds = None
-
-        jpgOut = self.imageFileOut[:-4]
-        jpgOut = jpgOut + ".jpg"
-
-        translateoptions = gdal.TranslateOptions(
-            creationOptions=["WORLDFILE=YES"], format="JPEG"
-        )
-
-        gdal.Translate(jpgOut, self.imageFileOut, options=translateoptions)
-
-        os.remove(tempOut)
-
-    def createGcpShape(self):
-        self.rotationCoords = RotationCoords(self)
-
-        self.rotationCoords.setAarTransformationParams(
-            self.dataStoreGeoref.getAarTransformationParams()
-        )
-
-        vl = QgsVectorLayer("Point", "temporary_points", "memory")
-
-        pr = vl.dataProvider()
-        vl.startEditing()
-        pr.addAttributes(
-            [QgsField("UUID", QVariant.String), QgsField("type", QVariant.String)]
-        )
-
-        shpOut = self.imageFileOut[:-4]
-        shpOut = shpOut + "gcp_aar.shp"
-
-        for point in self.gcpPoints:
-            feat = QgsFeature()
-            feat.setGeometry(
-                QgsGeometry.fromPointXY(
-                    QgsPointXY(float(point["aar_x"]), float(point["aar_z"]))
-                )
-            )
-            feat.setAttributes([point["uuid"], "aar"])
-            pr.addFeature(feat)
-
-            feat = QgsFeature()
-
-            retObj = self.rotationCoords.rotationReverse(
-                float(point["aar_x"]), float(point["aar_z"]), True
-            )
-
-            feat.setGeometry(
-                QgsGeometry.fromPointXY(
-                    QgsPointXY(retObj["x_trans"], retObj["y_trans"])
-                )
-            )
-            feat.setAttributes([point["uuid"], "reverse_aar"])
-            pr.addFeature(feat)
-
-        vl.updateExtents()
-
-        vl.commitChanges()
-
-        QgsVectorFileWriter.writeAsVectorFormat(
-            vl,
-            shpOut,
-            "UTF-8",
-            QgsCoordinateReferenceSystem("EPSG:31468"),
-            "ESRI Shapefile",
-        )
